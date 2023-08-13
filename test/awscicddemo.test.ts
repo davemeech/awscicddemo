24c52f4bb13583485865f2aba04d908705943d0d
David Meech <davemeech@gmail.com>
2023-08-12 22:48:54 -0400

    Testing pipeline with passing tests.

--- test/awscicddemo.test.ts
+++ test/awscicddemo.test.ts
@@ -2,20 +2,29 @@ import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import * as Awscicddemo from '../lib/awscicddemo-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/awscicddemo-stack.ts
-
 describe('CI/CD CodePipeline', () => {
-    test('That it is created with the expected properties', () => {
-          const app = new cdk.App();
-            // WHEN
-          const stack = new Awscicddemo.AwscicddemoStack(app, 'MyTestStack');
-            // THEN
-          const template = Template.fromStack(stack);
-        
-          template.hasResourceProperties('AWS::SQS::Queue', {
-            VisibilityTimeout: 300
-          });
-        });
+
+  test('That it is sucessfully created', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new Awscicddemo.AwscicddemoStack(app, 'MyTestStack');
+    // THEN
+    const template = Template.fromStack(stack);
+
+    template.resourceCountIs('AWS::CodePipeline::Pipeline', 1);
+  });
 })
+
+describe('CI/CD CodeBuild Project', () => {
+
+  test('That it is sucessfully created', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new Awscicddemo.AwscicddemoStack(app, 'MyTestStack');
+    // THEN
+    const template = Template.fromStack(stack);
+
+    template.resourceCountIs('AWS::CodeBuild::Project', 2);
+  })
+});
 
