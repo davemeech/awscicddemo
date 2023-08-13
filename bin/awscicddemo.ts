8a15bfacc39560bc0c2c4f02be9d828cf0e0ea13
David Meech <davemeech@gmail.com>
2023-08-12 20:54:57 -0400

    Added demo pipeline

--- bin/awscicddemo.ts
+++ bin/awscicddemo.ts
@@ -5,17 +5,5 @@ import { AwscicddemoStack } from '../lib/awscicddemo-stack';
 
 const app = new cdk.App();
 new AwscicddemoStack(app, 'AwscicddemoStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: {account: '492023795884', region: 'us-east-1'}
 });
--- lib/awscicddemo-stack.ts
+++ lib/awscicddemo-stack.ts
@@ -1,16 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as pipelines from 'aws-cdk-lib/pipelines'
 
 export class AwscicddemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwscicddemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // AWS CI-CD Pipeline
+    const cicdPipeline = new pipelines.CodePipeline(this, 'demoPipeline', {
+      synth: new pipelines.ShellStep('Synth', {
+        input: pipelines.CodePipelineSource.gitHub('davemeech/awscicddemo', 'main'),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth',
+        ]
+      })
+    })
   }
 }
