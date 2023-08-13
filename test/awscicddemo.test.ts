ecb3413be3eb229c7fc35bc827377d714bb403dc
David Meech <davemeech@gmail.com>
2023-08-12 22:33:01 -0400

    Added npm run test to build pipeline

--- test/awscicddemo.test.ts
+++ test/awscicddemo.test.ts
@@ -1,17 +1,21 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Awscicddemo from '../lib/awscicddemo-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as Awscicddemo from '../lib/awscicddemo-stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/awscicddemo-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Awscicddemo.AwscicddemoStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
-});
+describe('CI/CD CodePipeline', () => {
+    test('That it is created with the expected properties', () => {
+          const app = new cdk.App();
+            // WHEN
+          const stack = new Awscicddemo.AwscicddemoStack(app, 'MyTestStack');
+            // THEN
+          const template = Template.fromStack(stack);
+        
+          template.hasResourceProperties('AWS::SQS::Queue', {
+            VisibilityTimeout: 300
+          });
+        });
+})
+
