cb6f456ec7be0f6e258e0d3e7dad0973d0cdae53
David Meech <davemeech@gmail.com>
2023-08-12 20:14:07 -0400

    Initialized cdk project

---
+++ test/awscicddemo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Awscicddemo from '../lib/awscicddemo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/awscicddemo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Awscicddemo.AwscicddemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
